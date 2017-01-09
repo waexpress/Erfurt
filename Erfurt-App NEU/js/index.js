eb9b818fb8f0e68feb73a56e4afcc8a080129488
Axel Schmidt <kontakt@wa-express.de>
2017-01-09 12:28:55 +0100

    erf
    123

--- Erfurt-App NEU/js/index.js
+++ Erfurt-App NEU/js/index.js
@@ -32,17 +32,15 @@ var app = {
   // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
   };
 
-  window.plugins.OneSignal
-    .startInit("971d115a-ad05-4a9b-b6a2-971dc9ada326", "175787334223")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
+  window.plugins.OneSignal.init("971d115a-ad05-4a9b-b6a2-971dc9ada326",
+                                 {googleProjectNumber: "175787334223"},
+                                 notificationOpenedCallback);
   
-  // Call syncHashedEmail anywhere in your app if you have the user's email.
-  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+  // Show an alert box if a notification comes in when the user is in your app.
+  window.plugins.OneSignal.enableInAppAlertNotification(true);
 }, false);
     },
     // deviceready Event Handler
