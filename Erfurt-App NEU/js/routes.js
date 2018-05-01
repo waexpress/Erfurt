103b9b932492e23e30428e9985d252c381ae3d36
Axel Schmidt <kontakt@wa-express.de>
2018-05-01 09:32:07 +0200

    123
    1234

---
+++ Erfurt-App NEU/js/routes.js
@@ -0,0 +1,71 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController.cameraTabDefaultPage', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/cameraTabDefaultPage.html',
+        controller: 'cameraTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.cartTabDefaultPage', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/cartTabDefaultPage.html',
+        controller: 'cartTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.cloudTabDefaultPage', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/cloudTabDefaultPage.html',
+        controller: 'cloudTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('tabsController.city', {
+    url: '/page5',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/city.html',
+        controller: 'cityCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.tab5DefaultPage', {
+    url: '/page6',
+    views: {
+      'tab5': {
+        templateUrl: 'templates/tab5DefaultPage.html',
+        controller: 'tab5DefaultPageCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
+
+
+});
